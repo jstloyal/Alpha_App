92a61f2718b3768c01f3dd508a0649cb6e7b4d49
jstloyal <jstloyalty@gmail.com>
2020-06-22 10:42:49 +0100

    Add partial and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
-
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created!"
@@ -20,6 +23,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated!'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted!'
+    redirect_to articles_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # root to: 'articles#index'
-  root "articles#new"
+  root "articles#index"
   resources :articles
 end
