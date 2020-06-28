181672f0e0bb4fdb237be915286b2a29a1654632
jstloyal <jstloyalty@gmail.com>
2020-06-28 13:29:02 +0100

    add secure password and attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_101543) do
+ActiveRecord::Schema.define(version: 2020_06_28_122413) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_101543) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
