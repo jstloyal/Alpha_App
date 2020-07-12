515c014ec0f4a08c5c23af4c91c735ffbf272400
jstloyal <jstloyalty@gmail.com>
2020-07-12 02:52:23 +0100

    Add category model and test plus style errors message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have logged out"
+    flash[:success] = "You have logged out successfully"
     redirect_to root_path
   end
 end
